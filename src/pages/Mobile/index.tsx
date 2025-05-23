058a1f594f038dcb3f30447429fc786342b28563
zhangzhang88 <68057668+zhangzhang88@users.noreply.github.com>
2025-05-23 19:12:04 +0800

    Update index.tsx

--- src/pages/Mobile/index.tsx
+++ src/pages/Mobile/index.tsx
@@ -68,8 +68,8 @@ const MobilePage: React.FC = () => {
   return (
     <div className="flex  w-screen flex-col">
       <section className="flex items-center justify-center py-2 shadow-md">
-        <img src={logo} className="mr-3 h-16 w-16" alt="Qwerty Learner Logo" />
-        <h1 className="text-2xl font-bold text-primary">Qwerty Learner</h1>
+        <img src={logo} className="mr-3 h-16 w-16" alt="未来已到 Logo" />
+        <h1 className="text-2xl font-bold text-primary">未来已到</h1>
       </section>
 
       <section className="relative">
